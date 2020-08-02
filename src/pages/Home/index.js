deacfdb07cdc6186acbfdeab5d5c69ca16e71d62
Iwry Rozemberg <iwryrozemberg@gmail.com>
2020-08-02 13:38:52 -0300

    Corringo homepage com busca de dados pelo server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getAllWithVideos } from '../../services/categorias';
 
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Default from '../../components/templates/Default';
-
-// import dataList from '../../data/dados_iniciais.json';
-
 function Home() {
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos);
+        setCategorias([...categoriasComVideos]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    console.log(categorias);
+  }, []);
+
   return (
     <Default>
-      {/* <BannerMain
-        videoTitle={dataList.categorias[0].videos[0].titulo}
-        url={dataList.categorias[0].videos[0].url}
-        videoDescription="Conheça a história mitológica do Deus dos mares"
-      />
-      {dataList.categorias.map((categoria, index) => (
-        <Carousel
-          key={categoria.titulo}
-          ignoreFirstVideo={index === 0}
-          category={categoria}
-        />
-      ))} */}
+      {categorias.length >= 1 && (
+        <>
+          <BannerMain
+            videoTitle={categorias[0].videos[0].titulo}
+            url={categorias[0].videos[0].url}
+            videoDescription="Conheça a história mitológica do Deus dos mares"
+          />
+          {categorias.map((categoria, index) => (
+            <Carousel
+              key={categoria.titulo}
+              ignoreFirstVideo={index === 0}
+              category={categoria}
+            />
+          ))}
+        </>
+      )}
     </Default>
   );
 }

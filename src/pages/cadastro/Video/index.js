ca02358af22a5b1c67c1f2990d9f5b5b831655f4
Iwry Rozemberg <iwryrozemberg@gmail.com>
2020-08-03 00:46:13 -0300

    Criando Form para cadastro de vídeo

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,13 +1,95 @@
-import React from 'react';
-import Default from '../../../components/templates/Default';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-// import { Container } from './styles';
+import useForm from '../../../hooks/useForm';
+import { create } from '../../../services/videos';
+import { getAll } from '../../../services/categorias';
+
+import { Form, FormGroup, FormHeader, ButtonGroup } from './styles';
+
+import Default from '../../../components/templates/Default';
+import FormField from '../../../components/FormField';
+import { Button } from '../../../components/Button';
 
 function Video() {
+  const initValues = {
+    categoria: '',
+    categoriaId: '',
+    titulo: '',
+    url: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const categoriasTitulo = categorias.map(({ titulo }) => titulo);
+  const { clearForm, modelValues, onChange } = useForm(initValues);
+  const history = useHistory();
+
+  useEffect(() => {
+    getAll()
+      .then((response) => {
+        setCategorias([...response]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Default>
-      <Link to="/cadastro/categoria">SÓ VAI</Link>
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const categoriaSelected = categorias.find(
+            (categoria) => categoria.titulo === modelValues.categoria
+          );
+          console.log(categoriaSelected);
+          if (categoriaSelected) {
+            const {url, titulo} = modelValues;
+            create({
+              categoriaId: categoriaSelected.id,
+              titulo: titulo,
+              url: url,
+            }).then(() => {
+              clearForm();
+              history.push('/');
+            });
+          }
+        }}
+      >
+        <FormGroup>
+          <FormHeader>Cadastro de Vídeo</FormHeader>
+          <FormField
+            type="text"
+            label="Título"
+            name="titulo"
+            id="titulo"
+            value={modelValues.titulo}
+            onChange={onChange}
+          />
+          <FormField
+            type="text"
+            label="URL"
+            name="url"
+            id="url"
+            value={modelValues.url}
+            onChange={onChange}
+          />
+          <FormField
+            type="select"
+            label="Categoria"
+            id="categoria"
+            name="categoria"
+            value={modelValues.categoria}
+            suggestions={categoriasTitulo}
+            onChange={onChange}
+          />
+          <ButtonGroup>
+            <Button as={Link} to="/cadastro/categoria" className="ButtonForm">
+              Nova Categoria
+            </Button>
+            <Button type="submit" className="ButtonForm ButtonSubmit">
+              Cadastrar
+            </Button>
+          </ButtonGroup>
+        </FormGroup>
+      </Form>
     </Default>
   );
 }
--- src/pages/cadastro/Video/styles.js
+++ src/pages/cadastro/Video/styles.js
@@ -1,5 +1,39 @@
 import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
+export const Form = styled.form``;
+
+export const FormGroup = styled.fieldset`
+  border-color: var(--primary);
+  margin-bottom: 30px;
+  position: relative;
+`;
+export const FormHeader = styled.legend`
+  /* font-family: Ubuntu; */
+  /* text-transform: uppercase; */
+  font-size: 30px;
+  font-weight: bold;
+  color: var(--blackLighter);
+`;
+
+export const ButtonGroup = styled.div`
+  flex: 1;
+  float: right;
   
+  .ButtonForm {
+    background: var(--blackLighter);
+    color: var(--black);
+    border-color: var(--blackLighter);
+  }
+  .ButtonSubmit {
+    background: var(--primary);
+    border-color: var(--primary);
+    color: var(--white);
+  }
+
+  .ButtonForm + .ButtonForm {
+    margin-left: 10px;
+  }
+
+
 `;
